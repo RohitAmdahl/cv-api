200ae1646903228a98fcfaeaa4fc07cec17306d5
rohitAmdahl <rohit_designer@outlook.com>
2024-04-06 23:33:20 +0200

    mongoDb  database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
@@ -12,8 +14,9 @@ const cvdata_routes = require("./routes/cvdata");
 // middleware to parse json data from request body to object in express app object
 app.use("/api/cvdata", cvdata_routes);
 
-const startServer = () => {
+const startServer = async () => {
   try {
+    await connectDB();
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
