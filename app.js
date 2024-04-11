dc778afd9636f231b43b9c566b01c3f60a44499b
rohitAmdahl <rohit_designer@outlook.com>
2024-04-11 23:12:53 +0200

    -edit the uri and port change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const connectDB = require("./db/connect");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
